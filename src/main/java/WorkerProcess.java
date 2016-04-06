11d4da7774335199be03d6d1015aeff50900c19b
Fabio Hisamoto <fabio@99taxis.com>
2016-04-06 13:51:44 -0300

    Adds logger.

--- src/main/java/WorkerProcess.java
+++ src/main/java/WorkerProcess.java
@@ -4,11 +4,8 @@ import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
-import org.quartz.SimpleTrigger;
 import org.quartz.Trigger;
 import org.quartz.impl.StdSchedulerFactory;
-
-import java.util.Date;
 
 import static org.quartz.JobBuilder.newJob;
 import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
@@ -19,17 +16,15 @@ public class WorkerProcess {
   class HelloJob implements Job {
     public void execute(JobExecutionContext context)
             throws JobExecutionException {
-
       System.out.println("Hello Quartz!");
     }
   }
 
   public static void main(String[] args) {
-
     JobDetail job = newJob(HelloJob.class)
             .withIdentity("job1", "group1")
             .build();
-    
+
     Trigger trigger = newTrigger().withIdentity("trigger1", "group1")
             .startNow()
             .withSchedule(simpleSchedule().withIntervalInSeconds(5).repeatForever())
