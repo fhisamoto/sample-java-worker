60f08208feeeb3be27a72f6e9662ea8480a78b0b
Fabio Hisamoto <fabio@99taxis.com>
2016-04-06 15:39:41 -0300

    Adds test other_worker.

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 worker: sh target/bin/worker
+other_worker: sh target/bin/other_worker
---
+++ src/main/java/OtherWorkerProcess.java
@@ -0,0 +1,5 @@
+public class OtherWorkerProcess {
+  public static void main(String[] args) {
+    System.out.println("foo");
+  }
+}
